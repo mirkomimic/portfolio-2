7f5e8fb5a8449fe754e64fb8cb4e7dc82cffc4cc
mirkoMac <mirkomimic88@gmail.com>
2024-06-18 09:10:41 +0200

    initial

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    
+    <v-main>
+      <Transition name="fade" appear>
+        <div>
+          <slot/>
+        </div>
+      </Transition>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script setup lang="ts">
+// import NavBar from '@/Components/NavBar.vue';
+
+
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
